3256ad0fd72bafc1878059f158d8a1a75cce4c01
GabrielSiqueiraGama <siqueiragamagabriel@gmail.com>
2024-06-09 10:38:17 -0300

    feat: :seedling: PUT and GET method

--- src/main/java/com/JavaCoffee/BackEndJC/controllers/CardapioController.java
+++ src/main/java/com/JavaCoffee/BackEndJC/controllers/CardapioController.java
@@ -1,7 +1,9 @@
 package com.JavaCoffee.BackEndJC.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,5 +25,15 @@ public class CardapioController {
 		Cardapio novoItem = new Cardapio(nome, preco, descricao, imagem);
 		cardapioRepository.save(novoItem);
 		return novoItem;
+	}
+	@GetMapping
+	public Iterable<Cardapio> obterItens(){
+		return cardapioRepository.findAll();
+	}
+	
+	@PutMapping
+	public Cardapio editarItem(Cardapio cardapio) {
+		cardapioRepository.save(cardapio);
+		return cardapio;
 	}
 }
