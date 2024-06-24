67eae48ec1fe2e9adf98f4105ff5e6de54f68504
GabrielSiqueiraGama <siqueiragamagabriel@gmail.com>
2024-06-23 21:30:23 -0300

    build: :hammer: adding lombok

--- src/main/java/com/JavaCoffee/BackEndJC/model/entities/Cardapio.java
+++ src/main/java/com/JavaCoffee/BackEndJC/model/entities/Cardapio.java
@@ -4,7 +4,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.*;
 
+@Getter
+@Setter
+@NoArgsConstructor
 @Entity(name = "cardapio")
 public class Cardapio {
     
@@ -17,55 +21,10 @@ public class Cardapio {
     private String descricao;
     private String imagem;
 
-    public Cardapio() {
-        super();
-    }
-
     public Cardapio(String nome, Double preco, String descricao, String imagem) {
         this.nome = nome;
         this.preco = preco;
         this.descricao = descricao;
-        this.imagem = imagem;
-    }
-
-    // Getters e Setters
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public Double getPreco() {
-        return preco;
-    }
-
-    public void setPreco(Double preco) {
-        this.preco = preco;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-
-    public String getImagem() {
-        return imagem;
-    }
-
-    public void setImagem(String imagem) {
         this.imagem = imagem;
     }
 }
