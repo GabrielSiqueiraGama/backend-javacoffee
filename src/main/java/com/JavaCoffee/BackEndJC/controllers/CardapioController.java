e763a0e3e481952917bef77aa2ccd0919c25a56c
GabrielSiqueiraGama <siqueiragamagabriel@gmail.com>
2024-08-23 23:23:09 -0300

    feat: :seedling: finding by id

--- src/main/java/com/JavaCoffee/BackEndJC/controllers/CardapioController.java
+++ src/main/java/com/JavaCoffee/BackEndJC/controllers/CardapioController.java
@@ -5,10 +5,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,6 +33,10 @@ public class CardapioController {
 	@GetMapping
 	public Iterable<Cardapio> obterItens(){
 		return cardapioRepository.findAll();
+	}
+	@GetMapping("{/id}")
+	public ResponseEntity<Cardapio> findByID(@PathVariable int id) {
+		return cardapioRepository.findById(id).map(record -> ResponseEntity.ok().body(record)).orElse(ResponseEntity.notFound().build());
 	}
 	
 	@PutMapping
