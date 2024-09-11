0fd9c5bf0d3a125c0efa721ea40fb8e82c3d1d71
GabrielSiqueiraGama <siqueiragamagabriel@gmail.com>
2024-09-10 23:52:15 -0300

    feat: :seedling: adding soft delete do logic

--- src/main/java/com/JavaCoffee/BackEndJC/model/entities/Cardapio.java
+++ src/main/java/com/JavaCoffee/BackEndJC/model/entities/Cardapio.java
@@ -1,5 +1,9 @@
 package com.JavaCoffee.BackEndJC.model.entities;
 
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+import org.hibernate.validator.constraints.Length;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import jakarta.persistence.Entity;
@@ -7,12 +11,15 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.*;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @Entity(name = "cardapio")
+@SQLDelete(sql = "UPDATE cardapio SET status = 'Inativo' WHERE id = ?")
 public class Cardapio {
     
     @Id
@@ -31,6 +38,11 @@ public class Cardapio {
     
     private String categoria;
 
+    @NotNull
+    @Length(max = 10)
+    @Pattern(regexp = "Ativo|Inativo")
+    private String status = "Ativo"; 
+    
 	public Cardapio(String nome, Double preco, String descricao, String imagem, String categoria) {
         this.nome = nome;
         this.preco = preco;
