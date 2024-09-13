e0ad0f1f654250345379b929a06bf25f3a9cb175
GabrielSiqueiraGama <siqueiragamagabriel@gmail.com>
2024-09-12 21:43:29 -0300

    feat: :seedling: adding DTO to Cardarpio Entity

---
+++ src/main/java/com/JavaCoffee/BackEndJC/dto/CardapioDTO.java
@@ -0,0 +1,16 @@
+package com.JavaCoffee.BackEndJC.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record CardapioDTO(
+		@JsonProperty("_id") int id,
+		@NotBlank String nome,
+		@NotBlank @NotNull double preco,
+		@NotBlank @NotNull String descricao,
+		@NotBlank @NotNull String imagem,
+		@NotBlank @NotNull String categoria) {
+
+}
