35f15773fd0c3b9e9678895bc138017130af53a4
GabrielSiqueiraGama <siqueiragamagabriel@gmail.com>
2025-02-03 11:47:18 -0300

    feat: :lock: adding userRepository

---
+++ src/main/java/com/JavaCoffee/BackEndJC/service/AuthorizationService.java
@@ -0,0 +1,22 @@
+package com.JavaCoffee.BackEndJC.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.JavaCoffee.BackEndJC.model.repositories.UserRepository;
+
+@Service
+public class AuthorizationService implements UserDetailsService {
+
+	@Autowired
+	UserRepository userRepository;
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		// TODO Auto-generated method stub
+		return userRepository.findByLogin(username);
+	}
+
+}
