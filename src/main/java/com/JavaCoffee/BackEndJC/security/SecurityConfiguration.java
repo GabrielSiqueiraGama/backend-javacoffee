b64e59e88ca39024076b5d5c221415f95781339f
GabrielSiqueiraGama <siqueiragamagabriel@gmail.com>
2025-02-04 21:26:51 -0300

    feat: :lock: Adding firsts roles

--- src/main/java/com/JavaCoffee/BackEndJC/security/SecurityConfiguration.java
+++ src/main/java/com/JavaCoffee/BackEndJC/security/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.JavaCoffee.BackEndJC.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -17,6 +18,9 @@ public class SecurityConfiguration {
 		return httpSecurity
 				.csrf(csrf -> csrf.disable())
 				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+				.authorizeHttpRequests(authorize -> authorize
+						.requestMatchers(HttpMethod.POST, "/cardapio").hasRole("ADMIN")
+						.anyRequest().authenticated())
 				.build();
 	}
 }
