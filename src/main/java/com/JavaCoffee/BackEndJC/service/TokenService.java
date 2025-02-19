4ea8d34e103344a31bc0fbf684bce336421b799b
GabrielSiqueiraGama <siqueiragamagabriel@gmail.com>
2025-02-18 21:34:30 -0300

    feat: :lock: token validation

--- src/main/java/com/JavaCoffee/BackEndJC/service/TokenService.java
+++ src/main/java/com/JavaCoffee/BackEndJC/service/TokenService.java
@@ -13,6 +13,7 @@ import com.JavaCoffee.BackEndJC.model.entities.User;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 
 @Service
 public class TokenService {
@@ -34,6 +35,19 @@ public class TokenService {
 		}
 	}
 	
+	public String validateToken(String token) {
+		try {
+			Algorithm algorithm = Algorithm.HMAC256(secret);
+			return JWT.require(algorithm)
+					.withIssuer("auth-api")
+					.build()
+					.verify(token)
+					.getSubject();
+			
+		} catch (JWTVerificationException exception) {
+			return "";
+		}
+	}
 	
 	private Instant getExpirationDate() {
 		return LocalDateTime.now().plusDays(2).toInstant(ZoneOffset.of("-03:00"));
